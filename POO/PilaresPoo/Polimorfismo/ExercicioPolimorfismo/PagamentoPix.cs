66cc03395a5fcec32fb5eeb8c75a3f27602c6852
Caroline <carol.sant2008@gmail.com>
2025-11-14 16:33:47 -0300

    Polimorfismo

---
+++ POO/PilaresPoo/Polimorfismo/ExercicioPolimorfismo/PagamentoPix.cs
@@ -0,0 +1,15 @@
+namespace ExercicioPolimorfismo
+{
+    public class PagamentoPix : Pagamento 
+    {
+        private float desconto = 5 ;
+
+        public float ValorCompra;
+
+        public override float CalcularTotal()
+        {
+            float valorDesconto = ValorCompra / 100 * desconto;
+            return ValorCompra = ValorCompra - valorDesconto;
+        }
+    }
+}
---
+++ POO/PilaresPoo/Polimorfismo/ExercicioPolimorfismo/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExercicioPolimorfismo;
+
+PagamentoPix compraPix = new PagamentoPix();
+compraPix.ValorCompra = 15000;
+float valorPagar = compraPix.CalcularTotal();
+Console.WriteLine($"Produto: Smartphone ");
+Console.WriteLine($"Preco: {compraPix.ValorCompra}");
+Console.WriteLine($"Forma de pagamento: pix");
+Console.WriteLine($"agamento com desconto: {valorPagar}");
+
+
+PagamentoCartao compraCartao = new PagamentoCartao();
+compraCartao.ValorCompra = 50;
+
+Console.WriteLine($"Produto: Caneca");
+Console.WriteLine($"Preco: {compraPix.ValorCompra}");
+Console.WriteLine($"Forma de pagamento: Cartao");
+
+Console.WriteLine($"agamento com Acrescimo: {compraCartao.CalcularTotal()}");
